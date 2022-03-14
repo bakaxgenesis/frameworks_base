10f9569df3c7472777f8934225bec6e1ae2e9246
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-03-14 08:50:25 +0000

    Keyguard: allow large clock on tablets
    This effectively allows large clock to be shown on tablets,
    while keeping it disabled for small screen devices.
    
    Change-Id: I5bd1cdead55bd11dd4d0684ff95986cbcfdf4757

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -3,6 +3,7 @@ package com.android.keyguard;
 import static com.android.keyguard.KeyguardStatusAreaView.TRANSLATE_X_CLOCK_DESIGN;
 import static com.android.keyguard.KeyguardStatusAreaView.TRANSLATE_Y_CLOCK_DESIGN;
 import static com.android.keyguard.KeyguardStatusAreaView.TRANSLATE_Y_CLOCK_SIZE;
+import static com.android.systemui.shared.recents.utilities.Utilities.isLargeScreen;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -146,7 +147,8 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
         if (mDisplayedClockSize != null) {
             boolean landscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
-            boolean useLargeClock = mDisplayedClockSize == LARGE && !landscape;
+            boolean useLargeClock = mDisplayedClockSize == LARGE &&
+                    (!landscape || isLargeScreen(mContext));
             updateClockViews(useLargeClock, /* animate */ true);
         }
     }
@@ -452,7 +454,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         }
         boolean landscape = getResources().getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
-        boolean useLargeClock = clockSize == LARGE && !landscape;
+        boolean useLargeClock = clockSize == LARGE && (!landscape || isLargeScreen(mContext));
 
         // let's make sure clock is changed only after all views were laid out so we can
         // translate them properly
