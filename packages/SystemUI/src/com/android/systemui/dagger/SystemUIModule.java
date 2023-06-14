69cc2df7afd61f04995ab1e414c830a35f8c46ff
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:23 +0300

    SystemUI: Restore wifi and cellular QS tiles
    Change-Id: Ibbbabf30be08c6852f6aa968d28de94ac29b0e71

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -61,6 +61,7 @@ import com.android.systemui.dump.DumpManager;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.flags.FlagDependenciesModule;
 import com.android.systemui.flags.FlagsModule;
+import com.android.systemui.genesis.GenesisModule;
 import com.android.systemui.inputmethod.InputMethodModule;
 import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyevent.data.repository.KeyEventRepositoryModule;
@@ -204,6 +205,7 @@ import javax.inject.Named;
         FlagsModule.class,
         FlagDependenciesModule.class,
         FooterActionsModule.class,
+        GenesisModule.class,
         InputMethodModule.class,
         KeyEventRepositoryModule.class,
         KeyboardModule.class,
